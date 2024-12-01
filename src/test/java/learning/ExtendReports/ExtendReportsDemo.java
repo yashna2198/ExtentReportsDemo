b776971d480bb87bfbd2509f781ac5bb5695b6f7
MAJETY YASHNA <myashna2198@gmail.com>
2024-12-01 22:16:25 +0530

    First Commit Dec 01 2024

---
+++ src/test/java/learning/ExtendReports/ExtendReportsDemo.java
@@ -0,0 +1,40 @@
+package learning.ExtendReports;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExtendReportsDemo {
+	ExtentReports extent;
+	
+	@BeforeTest
+	public void Reports() {
+		String path = System.getProperty("user.dir")+"\\Reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Report Demo");
+		reporter.config().setDocumentTitle("Demo chart");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Yashna");		
+		
+	}
+	
+	@Test
+	public void InitialDemo() {
+		ExtentTest test = extent.createTest("Initial Demo");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+		driver.quit();
+		test.fail("Results do not match");
+		extent.flush();
+	}
+
+}
